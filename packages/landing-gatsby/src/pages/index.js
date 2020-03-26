e5d1ae148762572c7c5848595d69d5f8069b5cd9
richardsimms <richard@rsimms.com>
2020-03-26 17:04:58 +1100

    first commit

---
+++ packages/landing-gatsby/src/pages/index.js
@@ -0,0 +1,54 @@
+import React, { Fragment } from 'react';
+import Sticky from 'react-stickynode';
+import { ThemeProvider } from 'styled-components';
+import { charityTheme } from 'common/src/theme/charity';
+import { ResetCSS } from 'common/src/assets/css/style';
+import { DrawerProvider } from 'common/src/contexts/DrawerContext';
+import Navbar from '../containers/Charity/Navbar';
+import DrawerSection from '../containers/Charity/DrawerSection';
+import BannerSection from '../containers/Charity/BannerSection';
+import FeatureSection from '../containers/Charity/FeatureSection';
+import WorkSection from '../containers/Charity/WorkSection';
+import HumanityBlock from '../containers/Charity/HumanityBlock';
+import PromotionBlock from '../containers/Charity/PromotionBlock';
+import DonateSection from '../containers/Charity/DonateSection';
+import ClientBlock from '../containers/Charity/ClientBlock';
+import Footer from '../containers/Charity/Footer';
+import {
+  GlobalStyle,
+  CharityWrapper,
+  ContentWrapper,
+} from '../containers/Charity/charity.style';
+import SEO from '../components/seo';
+
+export default () => {
+  return (
+    <ThemeProvider theme={charityTheme}>
+      <Fragment>
+        <SEO title="Charity" />
+        <ResetCSS />
+        <GlobalStyle />
+        {/* Start charity wrapper section */}
+        <CharityWrapper>
+          <Sticky top={0} innerZ={9999} activeClass="sticky-nav-active">
+            <Navbar />
+          </Sticky>
+          <DrawerProvider>
+            <DrawerSection />
+          </DrawerProvider>
+          <ContentWrapper>
+            <BannerSection />
+            <FeatureSection />
+            <HumanityBlock />
+            <PromotionBlock />
+            <DonateSection />
+            <WorkSection />
+            <ClientBlock />
+          </ContentWrapper>
+          <Footer />
+        </CharityWrapper>
+        {/* End of charity wrapper section */}
+      </Fragment>
+    </ThemeProvider>
+  );
+};
